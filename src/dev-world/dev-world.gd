c806cc86aa3b6664e9a18a928537160794f8f3e1
azmec <pokekights6702@gmail.com>
2021-03-09 20:19:37 -0600

    Closes issue 1.

--- src/dev-world/dev-world.gd
+++ src/dev-world/dev-world.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-const TEST_SEQUENCE_PATH: = "res://assets/dialogues/test-sequence.json"
 
 var _cycles: = 0
 
@@ -13,9 +12,6 @@ onready var fpsLabel: Label = $CanvasLayer/Main/FPSLabel
 
 func _ready() -> void:
 	DialogueGod.set_parent(mainUI)
-
-	yield(get_tree().create_timer(0.5), "timeout")
-	DialogueGod.queue_sequence_to_message_stack(TEST_SEQUENCE_PATH)
 
 	AStarPathfinder.foster_tilemap(boringTileMap)
 
