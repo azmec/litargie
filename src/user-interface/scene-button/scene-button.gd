22da9fc8bbf26c661b82684fe63ad48a3ddadbfd
azmec <pokekights6702@gmail.com>
2021-03-10 18:13:45 -0600

    Added main menu.

---
+++ src/user-interface/scene-button/scene-button.gd
@@ -0,0 +1,4 @@
+class_name SceneButton
+extends Button
+
+export var scene: String 
--- src/user-interface/start-sequence/start-sequence.gd
+++ src/user-interface/start-sequence/start-sequence.gd
@@ -5,11 +5,15 @@ class_name StartSequence
 extends Control
 
 const START_SEQUENCE: String = "res://assets/dialogues/start-sequence.json"
-const REWORK_SEQUENCE: String = "res://assets/dialogues/rework-sequence.json"
 
 func _ready() -> void:
 	DialogueGod.set_parent(self)
-	DialogueGod.queue_sequence_to_message_stack(REWORK_SEQUENCE)
+	DialogueGod.queue_sequence_to_message_stack(START_SEQUENCE)
 
 	var _dialogueBox: DialogueBox = DialogueGod.get_dialogueBox()
-	_dialogueBox.toggle_panel(false)
+	_dialogueBox.rect_position.y -= 50
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		self.queue_free()
+		DialogueGod._hide()
