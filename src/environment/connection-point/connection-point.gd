449155ff32f623661f4ddef408ecb7657ad70028
azmec <pokekights6702@gmail.com>
2021-03-15 19:29:43 -0500

    Link connection between two connection points and dialogue signaling.

--- src/environment/connection-point/connection-point.gd
+++ src/environment/connection-point/connection-point.gd
@@ -3,17 +3,36 @@ extends Node2D
 
 const LINK_SCENCE: = preload("res://src/props/link/link.tscn")
 
+export var sequence_path: String 
+
 var _current_link_instance: Line2D = null 
 
 onready var playerDetector: = $PlayerDetector
+onready var linkDetector: = $LinkDetector
 
 func _ready() -> void:
 	playerDetector.connect("object_entered_zone", self, "_on_playerDetector_entered_zone")
 	playerDetector.connect("object_left_zone", self, "_on_playerDetector_exited_zone")
 
+	linkDetector.connect("area_entered", self, "_on_linkDetector_area_entered")
+
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("toggle_meathook") and playerDetector.detects_body():
-		_create_link()
+	if Input.is_action_just_pressed("interact") and playerDetector.detects_body():
+		if linkDetector.detects_area():
+			print("AAA")
+			# SCUFFED CODE
+			var _detected_area = linkDetector.get_area()
+			var _detected_link = _detected_area.get_parent() 
+
+			if not _detected_link.is_linked():
+				if _detected_area.name == "StartingPointArea2D":
+					_detected_link.starting_point_parent = self
+				elif _detected_area.name == "EndingPointArea2D":
+					_detected_link.ending_point_parent = self
+
+				DialogueGod.queue_sequence_to_message_stack(sequence_path)
+		else:
+			_create_link()
 
 func _create_link() -> void:
 	if _current_link_instance:
@@ -25,4 +44,3 @@ func _create_link() -> void:
 	_link.global_position = self.global_position
 	_link.starting_point_parent = self
 	_link.ending_point_parent = playerDetector.get_body()
-
--- src/props/link/link.gd
+++ src/props/link/link.gd
@@ -4,7 +4,8 @@ extends Line2D
 var starting_point_parent: Node2D = null
 var ending_point_parent: Node2D = null
 
-var is_linked: bool = false
+onready var startingPointArea2D: = $StartingPointArea2D
+onready var endingPointArea2D: = $EndingPointArea2D
 
 func _ready() -> void:
 	pass
@@ -17,8 +18,21 @@ func _physics_process(_delta: float) -> void:
 		self.points[-1] = to_local(ending_point_parent.global_position) 
 		self.points[-1].y -= 8
 
+	startingPointArea2D.position = self.points[0]
+	endingPointArea2D.position = self.points[-1]
+
 func get_starting_point_position() -> Vector2:
 	return self.points[0]
 
 func get_working_point_position() -> Vector2:
 	return self.points[-1]
+	
+func is_player(point_parent) -> bool:
+	return point_parent is Player
+
+func is_connection_point(point_parent) -> bool:
+	# Scuff rating is off the charts
+	return "_current_link_instance" in point_parent
+
+func is_linked() -> bool:
+	return is_connection_point(starting_point_parent) and is_connection_point(ending_point_parent)
