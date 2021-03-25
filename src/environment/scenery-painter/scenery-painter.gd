640180d6b6f2b0534de26c2afdb350b1680e8186
azmec <pokekights6702@gmail.com>
2021-03-25 18:06:07 -0500

    Created SceneryPainter.

---
+++ src/environment/scenery-painter/scenery-painter.gd
@@ -0,0 +1,14 @@
+class_name SceneryPainter
+extends TileMap
+
+export var variable_scenery_path: String
+
+func _ready() -> void:
+	var variable_scene: = load(variable_scenery_path)
+	for cell in get_used_cells():
+		var new_scenery: Node = variable_scene.instance()
+		self.add_child(new_scenery)
+
+		new_scenery.global_position = self.map_to_world(cell)
+		# Erasing the cell so that we're not overlapping textures.
+		set_cell(cell.x, cell.y, -1)
