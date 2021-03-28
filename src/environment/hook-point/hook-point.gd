d5e237d522f24f225488a056361612a6feb38656
azmec <pokekights6702@gmail.com>
2021-03-28 12:57:21 -0500

    Minor hookpoint fixes.

--- src/environment/hook-point/hook-point.gd
+++ src/environment/hook-point/hook-point.gd
@@ -7,7 +7,7 @@ onready var animationPlayer: = $AnimationPlayer
 func _ready() -> void:
 	var _hookDetector_connected: = hookDetector.connect("area_entered", self, "_on_hookDetector_area_entered")
 
-func _on_hookDetector_connected(hook: Area2D) -> void:
+func _on_hookDetector_area_entered(hook: Area2D) -> void:
 	var local_hook_location: = to_local(hook.global_position)
 	var direction_to_hook = self.position.direction_to(local_hook_location)
 
