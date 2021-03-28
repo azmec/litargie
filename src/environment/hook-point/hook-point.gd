d8804fb27286ffcac611948be8890981e90e5927
azmec <pokekights6702@gmail.com>
2021-03-28 13:10:07 -0500

    Integrated HookPoint to levels.

--- src/environment/hook-point/hook-point.gd
+++ src/environment/hook-point/hook-point.gd
@@ -8,8 +8,12 @@ func _ready() -> void:
 	var _hookDetector_connected: = hookDetector.connect("area_entered", self, "_on_hookDetector_area_entered")
 
 func _on_hookDetector_area_entered(hook: Area2D) -> void:
-	var local_hook_location: = to_local(hook.global_position)
-	var direction_to_hook = self.position.direction_to(local_hook_location)
+	var direction_to_hook = self.position.direction_to(hook.global_position)
+
+	print("HOOK DIRECTION:" + str(direction_to_hook))
 
 	self.rotation = direction_to_hook.angle()
+
+	print("NEW ROTATION: " + str(self.rotation))
+
 	animationPlayer.play("turn")
