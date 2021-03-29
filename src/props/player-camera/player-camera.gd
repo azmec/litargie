5d08f053f77d705ec06ab79d2c7034b40ec883bd
azmec <pokekights6702@gmail.com>
2021-03-29 12:22:23 -0500

    Made camera looking at mouse togglable.

--- src/props/player-camera/player-camera.gd
+++ src/props/player-camera/player-camera.gd
@@ -1,6 +1,8 @@
 class_name PlayerCamera
 extends Camera2D
 
+var is_looking_at_mouse: bool = true 
+
 onready var _parent: = get_parent()
 
 onready var topLeft: Position2D = $Limits/TopLeft
@@ -10,6 +12,10 @@ func _ready():
 	_update_limits()
 
 func _physics_process(_delta: float) -> void:
+	if is_looking_at_mouse:
+		_look_at_mouse()
+
+func _look_at_mouse() -> void:
 	var mouse_position = get_local_mouse_position()
 	self.position = mouse_position / 2
 
