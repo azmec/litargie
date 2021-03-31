b048fd122a9578b5215bb0a29decc86f6f55872c
azmec <pokekights6702@gmail.com>
2021-03-31 12:56:11 -0500

    Chapter 2: Room 1.

--- src/environment/scenery-painter/scenery-painter.gd
+++ src/environment/scenery-painter/scenery-painter.gd
@@ -9,6 +9,6 @@ func _ready() -> void:
 		var new_scenery: Node = variable_scene.instance()
 		self.add_child(new_scenery)
 
-		new_scenery.global_position = self.map_to_world(Vector2(cell.x, cell.y + 1), true)
+		new_scenery.global_position = self.map_to_world(Vector2(cell.x, cell.y + 1))
 		# Erasing the cell so that we're not overlapping textures.
 		set_cell(cell.x, cell.y, -1)
