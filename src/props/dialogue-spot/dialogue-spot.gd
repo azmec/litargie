daa6bf826bc71e59ecee7a35cf9dc28c9f04ccf8
azmec <pokekights6702@gmail.com>
2021-04-01 13:05:41 -0500

    Integrated DialogueSpot.

--- src/props/dialogue-spot/dialogue-spot.gd
+++ src/props/dialogue-spot/dialogue-spot.gd
@@ -11,10 +11,12 @@ onready var collisionShape2D: = $CollisionShape2D
 func _ready() -> void:
 	var _a = self.connect("body_entered", self, "_on_body_entered")
 
+	collisionShape2D.shape.radius = radius 
+
 func _process(_delta: float) -> void:
 	if Engine.editor_hint:
 		collisionShape2D.shape.radius = radius 
-		
+
 func _on_body_entered(_body: PhysicsBody2D) -> void:
 	if _has_been_activated: return
 
