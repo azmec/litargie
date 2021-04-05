95e41b0f3223f143209a2c31aef04b165c224824
azmec <pokekights6702@gmail.com>
2021-04-05 18:58:50 -0500

    Added bullet(s) and spring SFX.

--- src/props/turret-objects/bullet/bullet.gd
+++ src/props/turret-objects/bullet/bullet.gd
@@ -5,9 +5,11 @@ export var speed: = 100
 var velocity: Vector2 = Vector2.ZERO 
 
 onready var worldDetector: Area2D = $WorldDetector
+onready var audioPlayer: = $VariableSFXPlayer
 
 func _ready() -> void:
 	var _a: = worldDetector.connect("body_entered", self, "_on_worldDetector_body_entered")
+	audioPlayer.play()
 
 func _physics_process(delta: float) -> void:
 	self.rotation_degrees = rad2deg(velocity.angle()) - 90 
