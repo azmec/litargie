71746e714de6a902cb936187863275f97822d713
azmec <pokekights6702@gmail.com>
2021-04-05 18:03:36 -0500

    Added spring sprite and animation.

--- src/props/spring/spring.gd
+++ src/props/spring/spring.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
 onready var area2D: = $Area2D
+onready var animationPlayer: = $AnimationPlayer
 
 func _ready() -> void:
 	var _a: = area2D.connect("body_entered", self, "_on_area2D_body_entered")
 
+# Hard coding for player. Probably not a good idea.
 func _on_area2D_body_entered(body: Player) -> void:
 	body.velocity.y = -body.JUMP_STRENGTH * 2
+	animationPlayer.play("spring")
