66acf10287db2f8f2075596c0991ad56efb9c947
azmec <pokekights6702@gmail.com>
2021-04-13 15:26:33 -0500

    Added master bus volume.

--- src/autoloads/settings/settings.gd
+++ src/autoloads/settings/settings.gd
@@ -6,12 +6,16 @@ const POSSIBLE_LANGUAGES = ["ENG", "SPN"]
 
 var language: String = "ENG"
 
+onready var master_id = AudioServer.get_bus_index("Master")
 onready var sfx_id = AudioServer.get_bus_index("SFX")
 onready var bgm_id = AudioServer.get_bus_index("BGM") 
 
 func set_language(desired_language: String) -> void:
 	if POSSIBLE_LANGUAGES.find(desired_language) == -1:
 		print_debug("Invalid language!")
+
+func _set_master_volume(new_volume: float) -> void:
+	_set_volume(master_id, new_volume) 
 
 func set_sfx_volume(new_volume: float) -> void:
 	_set_volume(sfx_id, new_volume)
