726b97967448a3de16f1539db19c27f6b713b1b8
azmec <pokekights6702@gmail.com>
2021-04-13 10:29:44 -0500

    Audio settings helper functions.

--- src/autoloads/settings/settings.gd
+++ src/autoloads/settings/settings.gd
@@ -3,8 +3,25 @@
 extends Node
 
 const POSSIBLE_LANGUAGES = ["ENG", "SPN"]
+
 var language: String = "ENG"
+
+onready var sfx_id = AudioServer.get_bus_index("SFX")
+onready var bgm_id = AudioServer.get_bus_index("BGM") 
 
 func set_language(desired_language: String) -> void:
 	if POSSIBLE_LANGUAGES.find(desired_language) == -1:
 		print_debug("Invalid language!")
+
+func set_sfx_volume(new_volume: float) -> void:
+	_set_volume(sfx_id, new_volume)
+
+func set_bgm_volume(new_volume: float) -> void:
+	_set_volume(bgm_id, new_volume)
+
+func _set_volume(bus_id: int, new_volume: float) -> void:
+	var db_volume: = linear2db(new_volume)
+	AudioServer.set_bus_volume_db(bus_id, db_volume)
+
+func _get_volume(bus_id: int) -> float:
+	return db2linear(AudioServer.get_bus_volume_db(bus_id))
