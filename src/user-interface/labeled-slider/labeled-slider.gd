b995384c619c20d11b4f99a7985f6035c0abee59
azmec <pokekights6702@gmail.com>
2021-04-13 20:46:55 -0500

    Added audio feedback to LabeledSlider.

--- src/user-interface/labeled-slider/labeled-slider.gd
+++ src/user-interface/labeled-slider/labeled-slider.gd
@@ -1,3 +1,6 @@
+# HSlider which as an exportable label above it and 
+# emits audible feedback when interacted with. 
+
 tool
 extends HSlider
 
@@ -6,7 +9,13 @@ export (String) var parameter: = ""
 onready var panel: = $Panel
 onready var label: = $Label
 
+onready var hoverSFX: = $Hover
+onready var pressedSFX: = $Pressed
+
 func _ready() -> void:
+	self.connect("value_changed", self, "_on_value_changed")
+	self.connect("mouse_entered", self, "_on_mouse_entered") 
+
 	panel.rect_size.x = label.rect_size.x
 	label.text = parameter 
 
@@ -14,3 +23,9 @@ func _process(_delta: float) -> void:
 	if Engine.editor_hint:
 		panel.rect_size.x = label.rect_size.x
 		label.text = parameter
+
+func _on_value_changed(_value: float) -> void:
+	pressedSFX.play()
+
+func _on_mouse_entered() -> void:
+	hoverSFX.play()
