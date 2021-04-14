93bd1d6ae911747eec5ddb289003b04cd4c450cf
azmec <pokekights6702@gmail.com>
2021-04-13 20:35:51 -0500

    Added backButton.

--- src/user-interface/volume-sliders/volume-sliders.gd
+++ src/user-interface/volume-sliders/volume-sliders.gd
@@ -4,14 +4,21 @@
 
 extends VBoxContainer
 
+signal backRequested 
+
 onready var masterSlider: = $Master
 onready var sfxSlider: = $SFX
 onready var bgmSlider: = $BGM 
+
+# In any project worth its salt we wouldn't do this this way.
+onready var backButton: = $BackButton
 
 func _ready() -> void:
 	masterSlider.connect("value_changed", self, "_on_masterSlider_value_changed")
 	sfxSlider.connect("value_changed", self, "_on_sfxSlider_value_changed")
 	bgmSlider.connect("value_changed", self, "_on_bgmSlider_value_changed")
+
+	backButton.connect("pressed", self, "_on_backButton_pressed")
 
 func _on_masterSlider_value_changed(value: float) -> void:
 	Settings.master_volume = value
@@ -21,3 +28,6 @@ func _on_sfxSlider_value_changed(value: float) -> void:
 
 func _on__on_bgmSlider_value_changed(value: float) -> void:
 	Settings.bgm_volume = value
+
+func _on_backButton_pressed() -> void:
+	emit_signal("backRequested")
