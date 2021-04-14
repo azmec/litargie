f72a4e07c0a5150a8fffb861545ffc01931d4e7b
azmec <pokekights6702@gmail.com>
2021-04-13 20:56:10 -0500

    Created framework for FullMenuSet.

---
+++ src/user-interface/full-menu-set/full-menu-set.gd
@@ -0,0 +1,19 @@
+# There are a thousand good ways to create functional, 
+# fully traversable menus. This is not one of them. Godspeed. 
+
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
