1cb51a782b048364629c610582273bd5248a662e
azmec <pokekights6702@gmail.com>
2021-04-13 20:43:08 -0500

    Created AudibleButton.

---
+++ src/user-interface/audible-button/audible-button.gd
@@ -0,0 +1,17 @@
+# Extension of button that automatically plays "hover" or
+# "selected" sound effects when relevant. 
+
+extends Button
+
+onready var hoverSFX: = $Hover
+onready var pressedSFX: = $Pressed
+
+func _ready() -> void:
+	self.connect("pressed", self, "_on_pressed")
+	self.connect("mouse_entered", self," _on_mouse_entered") 
+
+func _on_pressed() -> void:
+	pressedSFX.play()
+
+func _on_mouse_entered() -> void:
+	hoverSFX.play()
