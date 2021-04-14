accbb9d1c7e8ab475dae210428e5cf9369675889
azmec <pokekights6702@gmail.com>
2021-04-13 20:40:10 -0500

    Framework for PrimaryMenu.

--- src/user-interface/primary-menu/primary-menu.gd
+++ src/user-interface/primary-menu/primary-menu.gd
@@ -1,2 +1,17 @@
 extends VBoxContainer
 
+signal playRequested()
+signal settingsRequested() 
+
+onready var playButton: = $PlayButton
+onready var settingsButton: = $SettingsButton 
+
+func _ready() -> void:
+	playButton.connect("pressed", self, "_on_playButton_pressed")
+	settingsButton.connect("pressed", self, "_on_settingsButton_pressed")
+
+func _on_playButton_pressed() -> void:
+	emit_signal("playRequested") 
+
+func _on_settingsButton_pressed() -> void:
+	emit_signal("settingsRequested")
