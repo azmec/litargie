bc741f84734a3d94852036ed47a28bf00139289d
azmec <pokekights6702@gmail.com>
2021-04-15 15:38:11 -0500

    Sliding PauseMenu on pressing "pause."

--- src/user-interface/full-menu-set/full-menu-set.gd
+++ src/user-interface/full-menu-set/full-menu-set.gd
@@ -5,6 +5,9 @@ extends Control
 
 signal playButton_pressed() 
 
+signal pause_requested()
+signal pause_exited()
+
 onready var settingsMenu: = $SettingsMenu
 onready var settingsBackButton: = $SettingsMenu/Sliding/VolumeSliders/BackButton
 
@@ -14,15 +17,27 @@ onready var mainMenuSettingsButton: = $MainMenu/Sliding/PrimaryMenu/SettingsButt
 onready var pauseMenu: = $PauseMenu
 
 func _ready() -> void:
-	mainMenu.connect("slide_completed", self, "_on_mainMenu_slide_completed")
+	mainMenu.connect("slide_completed", self, "_slide_completed")
+	pauseMenu.connect("slide_completed", self, "_on_slide_completed")
+	
 	mainMenuPlayButton.connect("pressed", self, "_on_mainMenuPlayButton_pressed")
 	mainMenuSettingsButton.connect("pressed", self, "_on_mainMenuSettingsButton_pressed")
 	settingsBackButton.connect("pressed", self, "_on_settingsBackButton_pressed")
 
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("pause") and _all_offscreen():
+		self.visible = true
+		pauseMenu.slide_onscreen()
+
+		emit_signal("pause_requested")
+	elif Input.is_action_just_pressed("pause") and not pauseMenu.is_offscreen():
+		pauseMenu.slide_offscreen()
+		emit_signal("pause_exited")
+	
 func _all_offscreen() -> bool:
 	return mainMenu.is_offscreen() and settingsMenu.is_offscreen() and pauseMenu.is_offscreen()
 
-func _on_mainMenu_slide_completed() -> void:
+func _on_slide_completed() -> void:
 	# If all of these are offscreen then we know we're
 	# just playing the game.
 	if _all_offscreen():
