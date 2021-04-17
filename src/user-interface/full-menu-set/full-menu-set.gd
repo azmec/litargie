cc2614894a28ef9dc162aa647443953e2776f03c
azmec <pokekights6702@gmail.com>
2021-04-17 13:18:06 -0500

    Fixed Settings removing Pause effect.

--- src/user-interface/full-menu-set/full-menu-set.gd
+++ src/user-interface/full-menu-set/full-menu-set.gd
@@ -68,11 +68,6 @@ func _switch(menuA: SlidingMenu, menuB: SlidingMenu) -> void:
 	_current_menu = menuB
 
 func _on_slide_completed() -> void:
-	if pauseMenu.is_offscreen() and _previous_menu == pauseMenu:
-		emit_signal("pause_exited")
-	elif not pauseMenu.is_offscreen() and _current_menu == pauseMenu:
-		emit_signal("pause_requested")
-		
 	# If all of these are offscreen then we know we're
 	# just playing the game.
 	if _all_offscreen():
